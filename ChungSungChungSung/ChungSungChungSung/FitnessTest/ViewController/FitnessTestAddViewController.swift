48fffd6ac4544707350d7006d5d4e003cdbf2671
Rain <eunbkang@gmail.com>
2022-08-05 20:45:19 +0900

    [Wip] working on FitnessTest add page

---
+++ ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessTestAddViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FitnessTestAddViewController.swift
+//  ChungSungChungSung
+//
+//  Created by Eunbee Kang on 2022/08/05.
+//
+
+import UIKit
+
+class FitnessTestAddViewController: UIViewController {
+    
+    @IBOutlet var addingCases: UISegmentedControl!
+    @IBOutlet var isRunning3km: UISwitch!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
