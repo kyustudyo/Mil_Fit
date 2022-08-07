cc730355d04c7992af240017768900487c7e5307
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-08-07 23:27:01 +0900

    Docs: 주석 제거

--- ChungSungChungSung/ChungSungChungSung/Main/ViewController/MainViewController.swift
+++ ChungSungChungSung/ChungSungChungSung/Main/ViewController/MainViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class MainViewController: UIViewController {
 
     var 전역가까움: Float = 0.9
+    let eatPercent = 70.0
+    let workoutPercent = 45.0
+    let basicPercent = 40.0
     
     fileprivate let dateLabel: UILabel = {
         let label = UILabel()
@@ -29,7 +32,6 @@ class MainViewController: UIViewController {
         bar.progressTintColor = UIColor.systemGray
         bar.setHeight(height: 30)
         bar.progressTintColor = .gray
-        
         bar.clipsToBounds = true
         bar.layer.cornerRadius = 16
         bar.layer.sublayers![1].cornerRadius = 16// 뒤에 있는 회색 track
@@ -66,10 +68,6 @@ class MainViewController: UIViewController {
         label.text = "더보기"
         return label
     }()
-    
-    let eatPercent = 70.0
-    let workoutPercent = 45.0
-    let basicPercent = 40.0
     
     fileprivate let calLabel: UILabel = {
         let label = UILabel()
@@ -122,7 +120,7 @@ class MainViewController: UIViewController {
     
     private enum Constants {
         static let lineSpacing = 8.0
-        static let purposeCellHeight = 105.0
+        static let purposeCellHeight = 115.0
         static let mealCellHeight = 185.0
         static let collectionViewContentInset = UIEdgeInsets(top: 10, left: 6, bottom: 10, right: 6)
       }
@@ -169,51 +167,37 @@ class MainViewController: UIViewController {
         let scrollView = UIScrollView()
         view.addSubview(scrollView)
         scrollView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
-        
         let emptyView = UIView()
         emptyView.backgroundColor = .white
         scrollView.addSubview(emptyView)
-        
         emptyView.anchor(top: scrollView.topAnchor, left: scrollView.leftAnchor,bottom: scrollView.bottomAnchor ,right: scrollView.rightAnchor, paddingTop: 0, paddingLeft: 0,paddingBottom: 0 ,paddingRight: 0)
-
         emptyView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
-        
-        
-        
+    
         //MARK: (날짜 + 멘트)
         let stack = UIStackView(arrangedSubviews: [dateLabel, todayMent])
         stack.axis = .vertical
-//        view.addSubview(stack)
         emptyView.addSubview(stack)
-//        stack.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 0, paddingLeft: 16, paddingRight: 16)
         stack.anchor(top: emptyView.safeAreaLayoutGuide.topAnchor, left: emptyView.safeAreaLayoutGuide.leftAnchor, right: emptyView.safeAreaLayoutGuide.rightAnchor, paddingTop: 0, paddingLeft: 16, paddingRight: 16)
         
         //MARK: 전역일 프로그레스
-//        view.addSubview(endDayBar)
+
         emptyView.addSubview(endDayBar)
         endDayBar.anchor(top:stack.bottomAnchor, left: stack.leftAnchor, right: stack.rightAnchor,paddingTop: 32, paddingLeft: 0, paddingRight: 0)
         endDayBar.progress = 전역가까움
-        
-//        view.addSubview(전역일)
         emptyView.addSubview(전역일)
         전역일.centerY(inView: endDayBar)
         전역일.anchor(left: endDayBar.leftAnchor, paddingLeft: 8)
-        
-//        view.addSubview(dDay)
         emptyView.addSubview(dDay)
-//        dDay.anchor(bottom: endDayBar.topAnchor, right: view.safeAreaLayoutGuide.leftAnchor,paddingBottom: 8, paddingRight: 전역가까움 > 0.1 ? -CGFloat(전역가까움) * (UIScreen.main.bounds.width - 16) : -60)
         dDay.anchor(bottom: endDayBar.topAnchor, right: emptyView.safeAreaLayoutGuide.leftAnchor,paddingBottom: 8, paddingRight: 전역가까움 > 0.1 ? -CGFloat(전역가까움) * (UIScreen.main.bounds.width - 16) : -60)
         
         //MARK: (목표들)
         let purposeStack = UIStackView(arrangedSubviews: [purposeLabel, purposeCollectionView])
         purposeStack.axis = .vertical
         purposeStack.spacing = 8
-//        view.addSubview(purposeStack)
         emptyView.addSubview(purposeStack)
         purposeStack.anchor(top: endDayBar.bottomAnchor, left: stack.leftAnchor, right: stack.rightAnchor, paddingTop: 16, paddingLeft: 0, paddingRight: 0)
         purposeCollectionView.anchor(left: stack.leftAnchor, right: stack.rightAnchor, paddingLeft: 0, paddingRight: 0)
         purposeCollectionView.setHeight(height: Constants.purposeCellHeight)
-        
         //MARK: 운동 달력
         
         //MARK: 식단
@@ -222,36 +206,24 @@ class MainViewController: UIViewController {
         let mealStack = UIStackView(arrangedSubviews: [mealHStack, mealCollectionView])
         mealStack.axis = .vertical
         mealStack.spacing = 8
-//        view.addSubview(mealStack)
         emptyView.addSubview(mealStack)
         mealStack.anchor(top: purposeStack.bottomAnchor, left: stack.leftAnchor, right: stack.rightAnchor, paddingTop: 16, paddingLeft: 0, paddingRight: 0)
         mealCollectionView.anchor(left: stack.leftAnchor, right: stack.rightAnchor, paddingLeft: 0, paddingRight: 0)
         mealCollectionView.setHeight(height: Constants.mealCellHeight)
         
         //MARK: 칼로리
-//        view.addSubview(eatRoundedView)
         
         emptyView.addSubview(calLabel)
         calLabel.anchor(top: mealStack.bottomAnchor, left: stack.leftAnchor, right: stack.rightAnchor, paddingTop: 16, paddingLeft: 0, paddingRight: 0)
-        
         let calContainerView = UIView()
         calContainerView.layer.cornerRadius = 16
         emptyView.addSubview(calContainerView)
         calContainerView.backgroundColor = .systemGray6
         calContainerView.anchor(top: calLabel.bottomAnchor, left: stack.leftAnchor, bottom: emptyView.bottomAnchor,right: stack.rightAnchor, paddingTop: 8, paddingLeft: 0, paddingBottom: 16,paddingRight: 0, height: 160)
-        
-        
-        
-//        emptyView.addSubview(eatRoundedView)
         calContainerView.addSubview(eatRoundedView)
         eatRoundedView.anchor(top: calContainerView.topAnchor, left: calContainerView.leftAnchor, right: calContainerView.rightAnchor, paddingTop: 16, paddingLeft: 8, paddingRight: 8, height: 16)
-        
-//        view.addSubview(workoutRoundedView)
-//        emptyView.addSubview(workoutRoundedView)
         calContainerView.addSubview(workoutRoundedView)
         workoutRoundedView.anchor(top: eatRoundedView.bottomAnchor, left: eatRoundedView.leftAnchor, right: eatRoundedView.rightAnchor, paddingTop: 4, paddingLeft: 0, paddingRight: 0, height: 16)
-        
-//        view.addSubview(basicRoundedView)
         emptyView.addSubview(basicRoundedView)
         basicRoundedView.anchor(top: workoutRoundedView.topAnchor, left: eatRoundedView.leftAnchor, right: eatRoundedView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, height: 16)
         
@@ -259,9 +231,6 @@ class MainViewController: UIViewController {
         let workOutCal = shortRoundedView(color: .blue)
         let eatCal = shortRoundedView(color: .brown)
         let threeRoundedViewStack = UIStackView(arrangedSubviews: [eatCal, basicCal, workOutCal])
-
-//        view.addSubview(threeRoundedViewStack)
-//        emptyView.addSubview(threeRoundedViewStack)
         calContainerView.addSubview(threeRoundedViewStack)
         threeRoundedViewStack.distribution = .equalCentering
         threeRoundedViewStack.anchor(top: basicRoundedView.bottomAnchor, left: calContainerView.leftAnchor, right: calContainerView.rightAnchor, paddingTop: 16, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, height: 16)
@@ -271,8 +240,6 @@ class MainViewController: UIViewController {
         let eatMentVstack = shortMent(ment: "먹은 대사량", value: 1700)
         
         let threeShortMentStack = UIStackView(arrangedSubviews: [basicMentVstack, workoutMentVstack, eatMentVstack])
-//        view.addSubview(threeShortMentStack)
-//        emptyView.addSubview(threeShortMentStack)
         calContainerView.addSubview(threeShortMentStack)
         threeShortMentStack.distribution = .equalCentering
         threeShortMentStack.anchor(top: threeRoundedViewStack.bottomAnchor, left: calContainerView.leftAnchor, right: stack.rightAnchor, paddingTop: 4, paddingLeft: 8,paddingRight: 8)
