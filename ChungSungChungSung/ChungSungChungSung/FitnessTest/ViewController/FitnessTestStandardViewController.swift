2fff7e6634f2f708904400345ae2a7f772701a56
Ayden <kaitaro77@naver.com>
2022-08-09 15:15:28 +0900

    [FEAT] Table View 생성

---
+++ ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessTestStandardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTestStandardViewController.swift
+//  ChungSungChungSung
+//
+//  Created by Ayden on 2022/08/08.
+//
+
+import UIKit
+
+class FitnessTestStandardViewController: UIViewController {
+    @IBOutlet weak var fitnessTestRunningTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
