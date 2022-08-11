cb69097d7480401a28857a62751352489642bad2
Somin Park <ramgoods2@gmail.com>
2022-08-11 13:55:29 +0900

    [Feat] : 선그래프 추가

--- ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessHistoryViewController.swift
+++ ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessHistoryViewController.swift
@@ -10,22 +10,34 @@ import Charts
 
 class FitnessHistoryViewController: UIViewController {
     
+    @IBOutlet weak var lineChartView: LineChartView!
+    
+    var fitnessRecord = [18.0, 17.0, 11.0, 30.0, 21.0]
     override func viewDidLoad() {
         super.viewDidLoad()
-        var numbers: [Double] = []
-        var lineChartEntry = [ChartDataEntry]()
-        
-        for i in 0..<numbers.count {
-            let value = ChartDataEntry(x: Double(i), y: numbers[i])
-            lineChartEntry.append(value)
+        serChart(lineValues: fitnessRecord)
+    }
+    func serChart(lineValues: [Double]) {
+        var lineDataEntries: [ChartDataEntry] = []
+
+        for i in 0..<lineValues.count {
+            let lineDataEntry = ChartDataEntry(x: Double(i), y: lineValues[i])
+            lineDataEntries.append(lineDataEntry)
         }
+        let lineChartDataSet = LineChartDataSet(entries: lineDataEntries, label: "기록")
+        lineChartDataSet.colors = [CustomColor.mainPurple!]
+        lineChartDataSet.circleColors = [CustomColor.mainPurple!]
+        lineChartDataSet.lineWidth = 3
+        lineChartDataSet.circleHoleRadius = 3.0
+        lineChartDataSet.circleRadius = 5.0
         
-        let line1 = LineChartDataSet(entries: lineChartEntry, label: "Number")
-        line1.colors = [NSUIColor.blue]
-        
-        let data = LineChartData()
-        data.append(line1)
-        
-        LineChartView.data = data
+        let data = LineChartData(dataSet: lineChartDataSet)
+        lineChartView.data = data
+        lineChartView.rightAxis.enabled = false
+        lineChartView.drawGridBackgroundEnabled = false
+//        lineChartView.leftAxis.enabled = false
+        lineChartView.xAxis.enabled = false
+        lineChartView.legend.enabled = false
+        lineChartView.backgroundColor = .white
     }
 }
