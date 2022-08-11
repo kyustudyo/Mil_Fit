a077d1abd22d0e45c27b85b981381b28b70b0095
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-08-11 19:28:34 +0900

    cocoapods 관련 파일 삭제

---
+++ ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessHistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FitnessHistoryTableViewCell.swift
+//  ChungSungChungSung
+//
+//  Created by Somin Park on 2022/08/11.
+//
+
+import UIKit
+
+class FitnessHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ChungSungChungSung/ChungSungChungSung/Workout/View/WorkoutListCell.swift
@@ -0,0 +1,13 @@
+//
+//  WorkoutListCell.swift
+//  ChungSungChungSung
+//
+//  Created by Eunbee Kang on 2022/08/10.
+//
+
+import Foundation
+import UIKit
+
+class WorkoutListCell: UITableViewCell {
+    @IBOutlet weak var workoutTitle: UILabel!
+}
