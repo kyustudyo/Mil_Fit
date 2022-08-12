f0009a92d5a5644487ef67d416189ea932caae91
Ayden <kaitaro77@naver.com>
2022-08-12 15:36:37 +0900

    [ADD] 25세 기준 추가

--- ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessTestStandardViewController.swift
+++ ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessTestStandardViewController.swift
@@ -13,7 +13,6 @@ struct FitnessTestStandardRunning {
 }
 
 var fitnessTestStandardSports = ["3km 달리기", "팔굽혀펴기", "윗몸일으키기"]
-
 var fitnessTestStandardRunningList = [
     FitnessTestStandardRunning(grade: "특급", time: "12분 30초 이내"),
     FitnessTestStandardRunning(grade: "1급", time: "12분 31초~13분 32초"),
@@ -23,7 +22,6 @@ var fitnessTestStandardRunningList = [
 ]
 
 
-
 class FitnessTestStandardViewController: UIViewController {
     @IBOutlet weak var fitnessTestRunningTableView: UITableView!
     @IBOutlet weak var fitnessTestStandardAge: UILabel!
@@ -31,6 +29,10 @@ class FitnessTestStandardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        fitnessTestRunningTableView.layer.shadowColor = UIColor.blue.cgColor
+        fitnessTestRunningTableView.layer.shadowRadius = 20
+        fitnessTestRunningTableView.layer.shadowOffset = CGSize(width: 4, height: 4)
+        fitnessTestRunningTableView.layer.masksToBounds = false
     }
 }
 
@@ -43,6 +45,9 @@ extension FitnessTestStandardViewController: UITableViewDelegate, UITableViewDat
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "FitnessTestStandardTableViewCell", for: indexPath) as? FitnessTestStandardTableViewCell else { return UITableViewCell() }
         cell.textLabel?.text = fitnessTestStandardRunningList[indexPath.row].grade
         cell.detailTextLabel?.text = fitnessTestStandardRunningList[indexPath.row].time
+//        cell.layer.shadowColor = UIColor.blue.cgColor
+//        cell.layer.shadowRadius = 20
+//        cell.layer.shadowOffset = CGSize(width: 4, height: 4)
         return cell
     }
     
