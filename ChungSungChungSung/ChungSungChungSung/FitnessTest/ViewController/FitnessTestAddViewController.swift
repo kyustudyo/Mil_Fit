7260898cd56047056a063502965284c95f5ff8ab
Ayden <kaitaro77@naver.com>
2022-08-12 19:05:28 +0900

    [FEAT] 체력검정 등록화면 스토리보드 생성

---
+++ ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessTestAddViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FitnessTestAddViewController.swift
+//  ChungSungChungSung
+//
+//  Created by Ayden on 2022/08/12.
+//
+
+import UIKit
+
+class FitnessTestAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
