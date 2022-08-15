f2ef16cb0fe4027eca7283c7789514a38f392fc2
Somin Park <ramgoods2@gmail.com>
2022-08-15 15:35:33 +0900

    [Feat] Realm pod 파일 추가

--- ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessHistoryViewController.swift
+++ ChungSungChungSung/ChungSungChungSung/FitnessTest/ViewController/FitnessHistoryViewController.swift
@@ -59,15 +59,14 @@ class FitnessHistoryViewController: UIViewController {
     
 }
 
-extension FitnessHistoryViewController: UITableViewDataSource, UITableViewDelegate
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    <#code#>
-}
+extension FitnessHistoryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    <#code#>
-}
-{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
     
 }
 
