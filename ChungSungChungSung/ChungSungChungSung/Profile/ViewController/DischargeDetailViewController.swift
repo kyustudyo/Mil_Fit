ba0f8c113f79dd8f681f6fa4cfd95a7395de2ecb
Ayden <kaitaro77@naver.com>
2022-08-17 01:27:18 +0900

    [FEAT] 전역일 뷰 생성

---
+++ ChungSungChungSung/ChungSungChungSung/Profile/ViewController/DischargeDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DischargeDetailViewController.swift
+//  ChungSungChungSung
+//
+//  Created by Ayden on 2022/08/17.
+//
+
+import UIKit
+
+class DischargeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = CustomColor.bgGray
+    }
+
+}
